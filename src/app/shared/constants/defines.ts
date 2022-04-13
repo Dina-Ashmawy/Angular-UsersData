0a78ec5c3bbf5f01059b50e183fb986f1546ee30
Dina Ashmawy <dina.h.ashmawy@hotmail.com>
2022-04-13 12:16:29 +0200

    DEVELOP: Re allocate header component

--- src/app/shared/constants/defines.ts
+++ src/app/shared/constants/defines.ts
@@ -1,8 +1,5 @@
 export const JSON_PATHS = {
     USERKEYVALUECONFIG: {
-        RESULTS: '$.results',
-        NAME: '$.name',
-        AGE: '$.dob.age',
-        PICTURE: '$.picture',
+        RESULTS: '$.results'
     }
 }
